d4c8f4872aaf70a237a94cf66f98baf54d81e81b
powerdot <donpsix@gmail.com>
2021-04-03 10:17:11 +0300

    upd

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,7 @@ let path = require("path");
 
 module.exports = {
     outputDir: path.resolve(__dirname, 'docs'),
-    publicPath: './'
+    publicPath: process.env.NODE_ENV === 'production'
+    ? '/vue-block-editor/'
+    : '/'
 }
